253ecac21729003bc1c02e271d1779b537441622
Sofia Lombardi <sofia.lomba2002@gmail.com>
2026-09-02 12:56:15 +0200

    migliorato type safety e sintassi in storage_service.dart

--- allergy_alert/lib/services/storage_service.dart
+++ allergy_alert/lib/services/storage_service.dart
@@ -9,14 +9,18 @@ class StorageService {
   Future<void> saveSymptom(SymptomLog log) async {
     final prefs = await SharedPreferences.getInstance();
 
-    List<SymptomLog> currentLogs = await getSymptoms();
+    // 1. Recupera la lista dei sintomi salvati (se vuota, usa una lista nuova)
+    final currentLogs = await getSymptoms();
 
+    // 2. Aggiunge il nuovo sintomo in cima alla lista
     currentLogs.insert(0, log);
 
-    List<String> stringList = currentLogs
+    // 3. Converte la lista di oggetti SymptomLog in lista di stringhe JSON
+    final List<String> stringList = currentLogs
         .map((item) => json.encode(item.toMap()))
         .toList();
 
+    // 4. Salva la lista aggiornata
     await prefs.setStringList(_storageKey, stringList);
   }
 
@@ -29,7 +33,8 @@ class StorageService {
     }
 
     return stringList.map((item) {
-      final Map<String, dynamic> map = json.decode(item);
+      final Map<String, dynamic> map =
+          jsonDecode(item) as Map<String, dynamic>;
       return SymptomLog.fromMap(map);
     }).toList();
   }
